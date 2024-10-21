829ddf5dd5cb9418688206a8dd7607f5b5be4cc4
Timo Peters <coding@tipela.de>
2024-10-21 22:15:46 +0300

    added scan block

--- main.py
+++ main.py
@@ -16,18 +16,35 @@ import os
 load_dotenv()  # take environment variables from .env.
 
 
+block_scan = False
+
 from websockets.asyncio.server import  serve
 
 CONNECTIONS = set()
+async def handle_message(raw_data):
+    print("new message: " + raw_data)
+    data = json.loads(raw_data)
+    global block_scan
+    if data["command"] == "block":
+        print("Blocking scan: " + str(data["data"]))
+        block_scan = data["data"]
+
+
 async def register(websocket):
+    global block_scan
     CONNECTIONS.add(websocket)
-    print("Connected")
+    print("Connection opened")
     try:
-        await websocket.wait_closed()
+        while True:
+            print("Waiting for message...")
+            data = await websocket.recv()
+            await handle_message(data)
     finally:
+        print("finally")
         CONNECTIONS.remove(websocket)
 
 async def video():
+    global block_scan
     vs = cv2.VideoCapture(1)
     detector = cv2.barcode.BarcodeDetector()
 
@@ -38,6 +55,7 @@ async def video():
     succesfull_scans = 0
     failed_scans = 0
     while True:
+        if not block_scan:
             ret, frame = vs.read()
             if not ret:
                 broadcast(CONNECTIONS, json.dumps({"command": "error", "data": { "message": "Error accessing camera"}}))
@@ -58,12 +76,12 @@ async def video():
                     failed_scans = 0
                     succesfull_scans = 0
                     broadcast(CONNECTIONS, json.dumps({"command": "scan", "data": {"ean": ean}}))
-                    await asyncio.sleep(10)
+                    await asyncio.sleep(1)
                 if failed_scans > 10:
                     print("Failed to scan")
                     failed_scans = 0
                     succesfull_scans = 0
-            await asyncio.sleep(0.001)
+        await asyncio.sleep(0.001)
 
 async def main():
     server = await serve(register, "0.0.0.0", os.getenv("PORT"))
